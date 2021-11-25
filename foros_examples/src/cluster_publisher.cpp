255af31f60c0e6cb105d819946e37a3f447d1f6a
Wonguk Jeong <wonguk.jeong@42dot.ai>
2021-11-25 09:48:06 +0900

    foros_example/cluster_publisher: support test with given cluster size

--- foros_examples/src/cluster_publisher.cpp
+++ foros_examples/src/cluster_publisher.cpp
@@ -26,26 +26,34 @@
 using namespace std::chrono_literals;
 
 int main(int argc, char **argv) {
-  uint32_t id = 1;
   const std::string kClusterName = "test_cluster";
   const std::string kTopicName = "test_cluster_echo";
-  const std::vector<uint32_t> kClusterNodeIds = {1, 2, 3, 4};
 
   rclcpp::Logger logger = rclcpp::get_logger(argv[0]);
   logger.set_level(rclcpp::Logger::Level::Info);
 
-  if (argc >= 2) {
-    id = std::stoul(argv[1]);
-    if (id > 4 || id == 0) {
-      RCLCPP_ERROR(logger, "please use id out of 1, 2, 3, 4");
-      return -1;
-    }
+  if (argc < 3) {
+    RCLCPP_ERROR(logger, "Usage : %s {node ID} {size of cluster}", argv[0]);
+    return -1;
+  }
+
+  uint32_t id = std::stoul(argv[1]);
+  uint32_t cluster_size = std::stoul(argv[2]);
+  std::vector<uint32_t> cluster_node_ids;
+
+  if (id >= cluster_size) {
+    RCLCPP_ERROR(logger, "ID must be less than cluster size");
+    return -1;
+  }
+
+  for (uint32_t i = 0; i < cluster_size; i++) {
+    cluster_node_ids.push_back(i);
   }
 
   rclcpp::init(argc, argv);
 
   auto node = akit::failover::foros::ClusterNode::make_shared(kClusterName, id,
-                                                              kClusterNodeIds);
+                                                              cluster_node_ids);
 
   node->register_on_activated([&]() { RCLCPP_INFO(logger, "activated"); });
   node->register_on_deactivated([&]() { RCLCPP_INFO(logger, "deactivated"); });
