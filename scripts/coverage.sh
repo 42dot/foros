ae53e60b50676b75c597445efac42ae6dea4e0c7
Wonguk Jeong <wonguk.jeong@42dot.ai>
2021-12-01 10:05:19 +0900

    scripts: use gcovr not lcov for coverage test
    Signed-off-by: Wonguk Jeong <wonguk.jeong@42dot.ai>

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -9,7 +9,4 @@ fi
 colcon build --cmake-args -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage -g" || exit 1
 colcon test
 colcon test-result || exit 1
-lcov --rc lcov_branch_coverage=1 --capture --directory ./build/foros/CMakeFiles/foros.dir/ --output-file coverage/app.info || exit 1
-lcov -r coverage/app.info "/usr/*" "/opt/*" "*/foros_msgs/*" -o coverage/app-final.info || exit 1
-
-genhtml coverage/app-final.info --branch-coverage --output-directory coverage/ || exit 1
+gcovr --xml-pretty --exclude-unreachable-branches --print-summary -o coverage/report.xml --root . -e "foros/test" -e "foros_examples" -e "foros_msgs" -e "build" -e "install/foros_msgs" --html coverage/report.html --html-detail
